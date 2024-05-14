ef1f56c76504818d959d83fe7c73f10e98404700
WYHDDY <3456564906@qq.com>
2024-05-14 17:00:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -15,8 +15,13 @@
 # rm -rf feeds/packages/net/v2ray-geodata
 # git clone https://github.com/wyhdag/dns -b v5 package/emortal/mosdns
 # git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-git clone https://github.com/derisamedia/luci-theme-alpha-alk-net.git package/emortal/luci-theme-alpha-alk
+# git clone https://github.com/derisamedia/luci-theme-alpha-alk-net.git package/emortal/luci-theme-alpha-alk
 sed -i 's/Os/O2/g' include/target.mk
-sed -i 's/luci-theme-bootstrap/luci-theme-alphaproto/g' feeds/luci/collections/luci/Makefile
-sed -i "27a\echo \"DISTRIB_DESCRIPTION=\'$(TZ=UTC-8 date "+%Y-%m-%d") By:ImmortalWRT\'\" >> /etc/openwrt_release" package/emortal/default-settings/files/99-default-settings
-sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/$(shell date +%y-%m-%d)-$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/g' include/image.mk
+sed -i 's/192.168.2.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt-2.4G/WYH-WIFI2.4G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i 's/ImmortalWrt-5G/WYH-WIFI/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i 's/MT7981_AX3000_2.4G/WYH-WIFI2.4G/g' package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
+sed -i 's/MT7981_AX3000_5G/WYH-WIFI/g' package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
+# sed -i 's/luci-theme-bootstrap/luci-theme-alphaproto/g' feeds/luci/collections/luci/Makefile
+# sed -i "27a\echo \"DISTRIB_DESCRIPTION=\'$(TZ=UTC-8 date "+%Y-%m-%d") By:ImmortalWRT\'\" >> /etc/openwrt_release" package/emortal/default-settings/files/99-default-settings
+# sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/$(shell date +%y-%m-%d)-$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/g' include/image.mk
