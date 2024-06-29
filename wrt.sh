43b653b0a819c39c7a93e96e5a4d16e4208e4f25
WYHDDY <3456564906@qq.com>
2024-06-29 16:20:28 +0800

    Update wrt.sh

--- wrt.sh
+++ wrt.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 dir="package/network/services/dnsmasq/files/dhcp.conf"
